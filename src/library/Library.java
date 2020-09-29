304c64883286c223cc4348db48a1e43a1fbb144a
Lewis Thomson <lewisfalconer@outlook.com>
2020-09-29 16:39:02 +0100

    Added function to add item

--- src/library/Library.java
+++ src/library/Library.java
@@ -4,17 +4,20 @@ import java.util.ArrayList;
 
 public class Library {
 
-	
+	private int collectionSize;
 	private ArrayList<Items> libraryContents;
 	
 	public Library()
 	{
+		collectionSize = 0;
 		libraryContents = new ArrayList<Items>();
 	}
 	
-	public Items checkOutItem(int itemID) {
+	public Items checkOutItem(int itemID) 
+	{
 		Items toCheckout = null;
-		for(Items itemSearch : libraryContents) {
+		for(Items itemSearch : libraryContents) 
+		{
 			if(itemID == itemSearch.getItemID())
 			{
 				toCheckout = itemSearch;
@@ -25,12 +28,30 @@ public class Library {
 		return toCheckout;
 	}
 	
-	public void checkInItem(Items itemToReturn) {
-		if(!itemToReturn.getAvailable()) {
+	public void checkInItem(Items itemToReturn) 
+	{
+		if(!itemToReturn.getAvailable()) 
+		{
 			itemToReturn.setAvailable(true);
 			libraryContents.add(itemToReturn);
 		}
 		
+	}
+	
+	public void addItem(Items itemToAdd)
+	{
+		libraryContents.add(itemToAdd);
+		setCollectionSize();
+	}
+	
+	public int getCollectionSize()
+	{
+		return this.collectionSize;
+	}
+	
+	public void setCollectionSize() 
+	{
+		collectionSize++;
 	}
 }
 
