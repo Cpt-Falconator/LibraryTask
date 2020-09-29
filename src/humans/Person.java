98f66f297de14ec094035909309d0389051a9718
Lewis Thomson <lewisfalconer@outlook.com>
2020-09-29 16:57:36 +0100

    Started adding person functionality

--- src/humans/Person.java
+++ src/humans/Person.java
@@ -1,5 +1,29 @@
 package humans;
 
-public class Person {
+import java.util.ArrayList;
 
+import library.Items;
+import library.Library;
+
+public class Person {
+	private ArrayList<Items> personalCollection;
+	
+	//Add check here incase item is not valid to check out
+	public void requestItem(Library library, int itemID) {
+		personalCollection.add(library.checkOutItem(itemID));
+	}
+	
+	public void returnItem(Library library, int itemID)
+	{
+		Items itemToReturn = null;
+		for(Items itemSearch : personalCollection) 
+		{
+			if(itemID == itemSearch.getItemID())
+			{
+				itemToReturn = itemSearch;
+			}
+		}
+		personalCollection.remove(itemToReturn);
+		library.checkInItem(itemToReturn);
+	}
 }
--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,8 +1,14 @@
 package main;
 
+import humans.Person;
+import library.Library;
+
 public class Runner {
 
 	public static void main(String[] args) {
+		
+		Library falconCollection = new Library();
+		Person person1 = new Person();
 		
 		//Hello World!
 	}
